dfdc1cc55124fb393994efcf4832ee52ae0a0382
Juan Manuel Torres <veinte2000jtm23@gmail.com>
2021-10-08 12:45:29 +0200

    Arreglado problema con los acentos

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 include("controller.php");
 $controller = new Controller();
 
-// Miramos a ver si hay alguna acci�n pendiente de realizar
+// Miramos a ver si hay alguna acción pendiente de realizar
 if (!isset($_REQUEST['action'])) {
-// No la hay. Usamos la acci�n por defecto (mostrar el formulario de login)
+// No la hay. Usamos la acción por defecto (mostrar el formulario de login)
     $action = "showLoginForm";
 } else {
-// S� la hay. La recuperamos.
+// Sí la hay. La recuperamos.
     $action = $_REQUEST['action'];
 }
 
-// Ejecutamos el m�todo del controlador que se llame como la acci�n
+// Ejecutamos el método del controlador que se llame como la acción
 $controller->$action();
