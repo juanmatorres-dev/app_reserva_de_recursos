ff6d514ff7082e8c27fa08193da9f4b6a5b415f2
Juan Manuel Torres <veinte2000jtm23@gmail.com>
2021-10-08 14:00:52 +0200

    Nuevo borrado de archivos

--- views/footer.php
+++
@@ -1,6 +0,0 @@
-    </div>
-    <div class='footer'>
-        Este es el pie
-    </div
-</body>
-</html>
--- views/mainMenu.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "Menú principal<br>";
-foreach ($data['permissions'] as $permission) {
-    echo "<a href='index.php?action=" . $permission->action . "'>" . $permission->description . "</a><br>";
-}
-echo "<a href='index.php?action=closeSession'>Cerrar sesión</a>";
