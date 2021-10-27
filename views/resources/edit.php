7b45f1beaebf70ff1fe4dd685d3bebd9900aabd3
Juan Manuel Torres <veinte2000jtm23@gmail.com>
2021-10-27 13:11:54 +0200

    ⬇ Edición de recursos en proceso ... ⬇
    - Ahora obtiene los datos actuales y los carga en el formulario

--- views/resources/edit.php
+++ views/resources/edit.php
@@ -6,23 +6,11 @@ echo "<h1>hola edit</h1>";
 
 foreach ($data['resources'] as $resources) {
 
-  echo "<tr>";
-  echo "<td>" . $resources['name'] . "</td>";
-  echo "<td>" . $resources['description'] . "</td>";
-  echo "<td>" . $resources['location'] . "</td>";
-  echo "<td>" . "<img src='" . $resources['image'] . "' width='50px'/>" . "</td>";
-
-  echo "<td><a class='btn btn-outline-info' href='index.php?controller=ResourcesController&action=editResource&id=" . $resources['id'] . "'>Modificar</a></td>";
-  echo "<td><a class='btn btn-outline-danger confirmacion' href='index.php?controller=ResourcesController&action=deleteResource&id=" . $resources['id'] .  "'>Borrar</a></td>";
-  echo "</tr>";
-}
-
-
-echo "<form enctype='multipart/form-data' action = 'index.php' method = 'POST'>
-				    <input type='hidden' name='id' value='$id'>
-                    Nombre:<input class='form-control' type='text' name='name' value='$resources['name']' required><br>
-                    Descripción:<input class='form-control' type='text' name='description' value='$description' required><br>
-                    Localización:<input class='form-control' type='text' name='location' value='$location' required><br>
+  echo "<form enctype='multipart/form-data' action = 'index.php' method = 'POST'>
+				    <input type='hidden' name='id' value='" . $resources['id'] . "'>
+                    Nombre:<input class='form-control' type='text' name='name' size='85' value='" . $resources['name'] . "' required><br>
+                    Descripción:<input class='form-control' type='text' name='description' size='85' value='" . $resources['description'] . "' required><br>
+                    Localización:<input class='form-control' type='text' name='location' size='85' value='" . $resources['location'] . "' required><br>
                     <input type='hidden' name='MAX_FILE_SIZE' value='500000' />
                     Imagen:<input class='form-control' type='file' name='image' required><br/>";
 
@@ -31,3 +19,9 @@ echo "  <input type='hidden' name='action' value='editResource'>
   <input class='btn btn-outline-primary' type='submit' value='Modificar'>
 </form>";
 echo "<br/><p><a class='btn btn-primary btn-sm' href='index.php'>Volver</a></p>";
+
+
+}
+
+
+
